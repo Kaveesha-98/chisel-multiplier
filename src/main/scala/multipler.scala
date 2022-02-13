78512368c90b5589dbfb17fa5aa1426d504bc1c1
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-02-13 19:21:26 +0530

    multiplier stage 2

--- src/main/scala/multipler.scala
+++ src/main/scala/multipler.scala
@@ -63,8 +63,44 @@ class multiplier extends Module {
     var stage2_adders_set2 = new Array[csa_3_input_n_bit](4)
     
     //ouput of stage2_adders_set1
+    val stage2_adders_set1_Sout = Wire(Vec(4, UInt(32.W)))
+    val stage2_adders_set1_Cout = Wire(Vec(4, UInt(32.W)))
+    
+    //output of stage2_adders_set2
     val stage2_adders_set2_Sout = Wire(Vec(4, UInt(32.W)))
-    val stage2_adders_set2_Cout = Wire(Vec(4, UInt(32.W)))
+    val stage2_adders_set2_Cout = Wire (Vec(4, UInt(32.W)))
+    
+    //output of stage2_adders
+    val stage2_adders_Sout = Wire(Vec(4, UInt(39.W)))
+    val stage2_adders_Cout = Wire (Vec(4, UInt(36.W)))
+    
+    for(i <- 0 to 3){
+    	stage2_adders_set1(i) = Module(new csa_3_input_n_bit(32))
+    	
+    	stage2_adders_set1(i).io.A := stage1_adders_Sout(i)(34, 3)
+    	stage2_adders_set1(i).io.B := stage1_adders_Cout(i)
+    	stage2_adders_set1(i).io.Cin := Cat(stage1_adders_Sout(i + 1)(30, 0), 0.U(1.W))
+    	
+    	stage2_adders_set1_Sout(i) := stage2_adders_set1(i).io.Sout
+    	stage2_adders_set1_Cout(i) := stage2_adders_set1(i).io.Cout
+    	
+    	stage2_adders_set2(i) = Module(new csa_3_input_n_bit(32))
+    	
+    	stage2_adders_set2(i).io.A := Cat(stage1_adders_Sout(i + 1)(34, 31), stage2_adders_set1_Sout(i)(31, 4))
+    	stage2_adders_set2(i).io.B := Cat(0.U(3.W), stage2_adders_set1_Cout(i)(31, 3))
+    	stage2_adders_set2(i).io.Cin := stage1_adders_Cout(i + 1)
+    	
+    	stage2_adders_set2_Sout(i) := stage2_adders_set2(i).io.Sout
+    	stage2_adders_set2_Cout(i) := stage2_adders_set2(i).io.Cout
+    	
+    	stage2_adders_Sout(i) := Cat(stage2_adders_set2_Sout(i), Cat(stage2_adders_set1_Sout(i)(3, 0), stage1_adders_Sout(i)(2, 0)))
+    	stage2_adders_Cout(i) := Cat(stage2_adders_set2_Cout(i), Cat(0.U(1.W), stage2_adders_set1_Cout(i)(2, 0)))
+    }
+    
+    //adders for 2nd stage
+    var stage3_adders_set1 = new Array[csa_3_input_n_bit](2)
+    var stage3_adders_set2 = new Array[csa_3_input_n_bit](2)
+    
     
     
 	io.Sout := 1.U
