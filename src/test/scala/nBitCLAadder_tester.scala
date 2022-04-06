f76ef804a3765f4b95d0b5190c079dbf31180866
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-04-06 18:36:31 +0530

    full adder testing

--- src/test/scala/nBitCLAadder_tester.scala
+++ src/test/scala/nBitCLAadder_tester.scala
@@ -9,8 +9,24 @@ class nBitCLAadder_tester(dut: CLA_adder) extends
 	
 	poke(dut.io.A, 2.U)
 	poke(dut.io.B, 2.U)
-	poke(dut.io.Cin, 0.U)
+	poke(dut.io.Cin, 1.U)
 	step(1)
+	for(Cin <- 0 to 1){
+		for(A <- 0 to 255){
+			println(A.toString)
+			for(B <- 0 to 255){
+				poke(dut.io.A, A.U)
+				poke(dut.io.B, B.U)
+				poke(dut.io.Cin, Cin.U)
+				step(1)
+				if(peek(dut.io.overflow) == 1){
+					expect(dut.io.sum, A+B+Cin-256)
+				}else{
+					expect(dut.io.sum, A+B+Cin)
+				}
+			}
+		}
+	}
 	println (" Result is: " + peek(dut.io.sum).toString)
 }
 /*
@@ -28,7 +44,7 @@ object multiplier_tester extends App{
 }*/
 
 object nBitCLAadder_tester extends App{
-	chisel3.iotesters.Driver(() => new CLA_adder(32)){
+	chisel3.iotesters.Driver(() => new CLA_adder(8)){
 		c => new nBitCLAadder_tester(c)
 	}
 }
