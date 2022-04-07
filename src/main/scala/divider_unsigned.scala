52e8bf685ad90981728ea5c4434aaf9334be55ea
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-04-07 17:30:51 +0530

    unsigned divider

--- src/main/scala/divider_unsigned.scala
+++ src/main/scala/divider_unsigned.scala
@@ -4,9 +4,11 @@ import chisel3.Driver
 
 class unsigned_divider_32bit extends Module {
     val io = IO(new Bundle{
-        val divider = Input(UInt(32.W))
-        val dividend = Input(UInt(32.W))
-        val valid = Input(Bool())
+        val divider     = Input(UInt(32.W))
+        val dividend    = Input(UInt(32.W))
+        val valid       = Input(Bool())
+        val quotient    = Output(UInt(32.W))
+        val remainder   = Output(UInt(32.W))
     })
 
     def addUnsignedInts[T <: Data](int_a: T, int_b: T, carry_in: T, width: Int): chisel3.UInt = {
@@ -21,25 +23,86 @@ class unsigned_divider_32bit extends Module {
 
     }
 
+    val decrement = -1.S.asUInt
+
+    val inWireDivisorAddAdd = Wire(UInt(35.W))
+    val inWireDivisorSubSub = Wire(UInt(35.W))
+    val inWireDivisorComp   = Wire(UInt(35.W))
+    val inWireDivisor       = Wire(UInt(35.W))
+    
+    inWireDivisorAddAdd := addUnsignedInts(Cat(0.U(1.W), io.divider, 0.U(1.W)), Cat(0.U(2.W), io.divider), 0.U, 34)
+    inWireDivisorSubSub := Cat(addUnsignedInts( Cat( 1.U(1.W), ~(io.divider) ), Cat( 3.U(2.W), ~(io.divider(31, 1)) ), 1.U, 33), ~(io.divider(1)) )
+    inWireDivisorComp   := addUnsignedInts(0.U, Cat( 3.U(2.W), ~(io.divider) ), 1.U, 34)
+    inWireDivisor       := Cat(0.U(3.W), io.divider)
+
     val ready :: running :: Nil = Enum(2)
     val stateReg = RegInit(ready)
+    val cntReg = Reg(UInt(6.W))
 
-    val quotient    = Reg(UInt(32.W))
-    val remainder   = Reg(UInt(32.W))
-    val divisor     = Reg(UInt(32.W))
+    val quotient    = Reg(UInt(34.W))
+    val remainder   = Reg(UInt(34.W))
+    val divisor     = Reg(UInt(34.W))
+    val divisorComp = Reg(UInt(34.W))
 
-    val add = addUnsignedInts(io.dividend, io.divider, 1.U, 32)
+    val divisorAddAdd = Reg(UInt(35.W))
+    val divisorSubSub = Reg(UInt(35.W))
+
+    val remainderRound1 = Cat(remainder(32, 0), quotient(33))
+    val remainderRound2 = Cat(remainder(32, 0), quotient(33, 32))
+
+    val round1DivisorOp = Mux(remainder(33).asBool, divisor, divisorComp)
+    val round1Result    = addUnsignedInts(remainderRound1, round1DivisorOp, 0.U, 34)
+
+    val round2DivisorOpAdd  = Wire(UInt(35.W))
+    val round2DivisorOpSub  = Wire(UInt(35.W))
+    val round2CarryIn       = Wire(UInt(1.W))
+
+    when(remainder(33).asBool){
+        round2DivisorOpAdd := divisorAddAdd
+        round2DivisorOpSub := Cat(0.U(1.W), divisor)
+        round2CarryIn := 0.U
+    }.otherwise{
+        round2DivisorOpAdd := Cat(1.U(1.W), divisorComp)
+        round2DivisorOpSub := divisorSubSub
+        round2CarryIn := 1.U
+    }
+
+    val round2ResultAdd = addUnsignedInts(remainderRound2, round2DivisorOpAdd, round2CarryIn, 35)
+    val round2ResultSub = addUnsignedInts(remainderRound2, round2DivisorOpSub, round2CarryIn, 35)
+
+    val round2Result = Mux(round1Result(33).asBool, round2ResultAdd, round2ResultSub)
+
+    val newQuotientBits = Cat(~round1Result(33), ~round2Result(34))
+
+    val nextQuotient    = Cat(quotient(31, 0), newQuotientBits)
+    val nextRemainder   = round2Result(33, 0)
+    val nextCount       = addUnsignedInts(cntReg, decrement, 0.U, 6)
 
     switch(stateReg){
         is(ready){
-            remainder   := 0.U
-            quotient    := io.dividend
-            divisor     := io.divider
+            remainder       := 0.U
+            quotient        := io.dividend
+            divisor         := inWireDivisor(33, 0)
+            divisorComp     := inWireDivisorComp(33, 0)
+            divisorAddAdd   := inWireDivisorAddAdd
+            divisorSubSub   := inWireDivisorSubSub
             when(io.valid){
                 stateReg := running
+                cntReg := 16.U
+            }
+        }
+        is(running){
+            cntReg      := nextCount(5, 0)
+            quotient    := nextQuotient
+            remainder   := nextRemainder
+            when(cntReg === 0.U){
+                stateReg := ready
             }
         }
     }
+
+    io.quotient     := quotient(31, 0)
+    io.remainder    := remainder(31, 0)
 }
 
 object unsigned_divider extends App{
