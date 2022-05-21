c556631d34a882acf7b4a555fd98d6d22e35c1cc
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-05-21 16:38:23 +0530

    commenting out lines

--- src/test/scala/unsigned_divider_tester.scala
+++ src/test/scala/unsigned_divider_tester.scala
@@ -4,15 +4,15 @@ import org.scalatest._
 import chisel3._
 import chisel3.Driver
 
-class unsigned_divider_32bit_tester(dut: unsigned_divider_32bit) extends
+class unsigned_divider_32bit_tester(dut: unsigned_divider_32bit, divisorCoeffient: BigInt, dividendCoeffcient: BigInt, threadName: String) extends
 	PeekPokeTester(dut){
 	
-	poke(dut.io.dividend, 11.U)
-	poke(dut.io.divider, 4.U)
+	poke(dut.io.dividend, "b11111111111111111111111111111111".U)
+	poke(dut.io.divider, "b00000000000000000000000000000001".U)
 	poke(dut.io.valid, true.B)
 	step(1)
 	poke(dut.io.valid, false.B)
-	step(30)
+	step(18)
 	/* for(Cin <- 0 to 1){
 		for(A <- 0 to 255){
 			println(A.toString)
@@ -31,6 +31,22 @@ class unsigned_divider_32bit_tester(dut: unsigned_divider_32bit) extends
 	} */
 	println (" Quotient is: " + peek(dut.io.quotient).toString)
 	println (" Remainder is: " + peek(dut.io.remainder).toString)
+
+	
+
+	/* for(divisor <- 1 to 255){
+		println("Thread number: " + threadName + ", currently working divisor: " + divisor.toString)
+		for(dividend <- 0 to 255){
+			poke(dut.io.dividend, (dividend*dividendCoeffcient).U)
+			poke(dut.io.divider, (divisor*divisorCoeffient).U)
+			poke(dut.io.valid, true.B)
+			step(1)
+			poke(dut.io.valid, false.B)
+			step(18)
+			expect(dut.io.quotient, (dividend*dividendCoeffcient)/(divisor*divisorCoeffient))
+			expect(dut.io.remainder, (dividend*dividendCoeffcient)%(divisor*divisorCoeffient))
+		}
+	} */
 }
 
 /*
@@ -47,8 +63,55 @@ object multiplier_tester extends App{
 	}
 }*/
 
+class Method1(divisorCoeffient: Int, dividendCoeffcient: Int) extends Thread {
+  override def run(): Unit ={
+    chisel3.iotesters.Driver.execute(Array("--target-dir", "generated", "--generate-vcd-output", "on"), () => new unsigned_divider_32bit()){
+		c => new unsigned_divider_32bit_tester(c, divisorCoeffient, dividendCoeffcient, this.getName)
+	}
+  }
+}
+
+/* object unsigned_divider_32bit_tester extends App {
+    val th0 = new Method1(1, 1)
+    th0.setName(0.toString)
+    val th1 = new Method1(1, 256)
+    th1.setName(1.toString)
+    val th2 = new Method1(1, 256*256)
+    th2.setName(2.toString)
+    val th3 = new Method1(1, 256*256*256)
+    th3.setName(3.toString)
+    val th4 = new Method1(256, 1)
+    th4.setName(4.toString)
+    val th5 = new Method1(256, 256)
+    th5.setName(5.toString)
+    /* val th6 = new Method1(256, 256*256)
+    th6.setName(6.toString)
+    val th7 = new Method1(256, 256*256*256)
+    th7.setName(7.toString)
+    val th8 = new Method1(256*256, 1)
+    th8.setName(8.toString)
+    val th9 = new Method1(256*256, 256)
+    th9.setName(9.toString)
+    val th10 = new Method1(256*256, 256*256)
+    th10.setName(10.toString)
+    val th11 = new Method1(256*256, 256*256*256)
+    th11.setName(11.toString) */
+    th0.start()
+    th1.start()
+    th2.start()
+    th3.start()
+    th4.start()
+    th5.start()
+    /* th6.start()
+    th7.start()
+    th8.start()
+    th9.start()
+    th10.start()
+    th11.start() */
+} */
+
 object unsigned_divider_32bit_tester extends App{
 	chisel3.iotesters.Driver.execute(Array("--target-dir", "generated", "--generate-vcd-output", "on"), () => new unsigned_divider_32bit()){
-		c => new unsigned_divider_32bit_tester(c)
+		c => new unsigned_divider_32bit_tester(c, 1, 256*256*256, "0")
 	}
 }
